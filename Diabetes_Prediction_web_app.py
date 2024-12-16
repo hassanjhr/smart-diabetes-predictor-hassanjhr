cd72b6efbc2e86a73bea4c6b26272c23b05c825d
Muhammad Hassan Jawaid <141501919+hassanjhr@users.noreply.github.com>
2024-12-16 16:18:02 +0500

    third commit

--- Diabetes_Prediction_web_app.py
+++ Diabetes_Prediction_web_app.py
@@ -10,7 +10,7 @@ import pickle
 import streamlit as st
 
 # Load the trained model
-loaded_model = pickle.load(open('C:/Users/Precision/Desktop/Diabetes_Prediction/trained_model.sav', 'rb'))
+loaded_model = pickle.load(open('trained_model.sav', 'rb'))
 
 # Function for Diabetes Prediction
 def diabetes_prediction(input_data):
